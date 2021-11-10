5e1f81849961e7927165317ba9e3e94b0976aab2
Christopher Neal <christopher.m.neal@gmail.com>
2021-11-10 14:34:10 -0800

    Move character[i] creators to character.js and update exports

--- __tests__/character.test.js
+++ __tests__/character.test.js
@@ -1,21 +1,9 @@
-import Character from '../src/js/character.js';
+import { Character } from '../src/js/character.js';
+// import { character1 } from '../src/js/character.js';
 
 describe ('Character', () => {
-  let character0;
-  let character1;
-  let character2;
-  let character3;
-  let character4;
-
-  beforeEach(() => {
-    character0 = new Character('freshman', 'nothing', 20)
-    character1 = new Character('jock', 'strength', 30);
-    character2 = new Character('nerd', 'robots', 10);
-    character3 = new Character('criminal', 'switchblade', 30);
-    character4 = new Character('artsy', 'intelligence', 15);
-  });
-
   test('should correctly create two characters using given characteristics', () => {
+    const character1 = new Character('jock', 'strength', 30);
     expect(character1.type).toEqual('jock');
     expect(character1.skill).toEqual('strength');
     expect(character1.hp).toEqual(30);
--- src/js/character.js
+++ src/js/character.js
@@ -1,4 +1,4 @@
-export default class Character {
+export class Character {
   constructor(type, skill, hp) {
     this.type = type;
     this.skill = skill;
@@ -6,39 +6,8 @@ export default class Character {
   }
 }
 
-
-// fight(instigator, instigatee) {
-//   let instigatorRoll = (Math.floor(Math.random()) * 20) +1;
-//   let minimumWinningRoll = 0;
-//   minimumWinningRoll = Math.floor(instigatee.hp/(instigator.hp + instigatee.hp)*20);
-
-  // if (instigator.includes(switchblade)) {
-  //   instigatorRoll += 2
-  // } else if (instigator.includes(strength)) {
-  //   instigatorRoll += 3
-  // } else if (instigator.includes(robots)) {
-  //   instigatorRoll += 5
-  // } else if (instigator.includes(intelligence)) {
-  //   instigatorRoll += 3
-  // } else { 
-  //   instigatorRoll;
-  // }
-
-  // if (instigatee.includes(switchblade)) {
-  //   minimumWinningRoll += 2
-  // } else if (instigatee.includes(strength)) {
-  //   minimumWinningRoll += 3
-  // } else if (instigatee.includes(robots)) {
-  //   minimumWinningRoll += 5
-  // } else if (instigatee.includes(intelligence)) {
-  //   minimumWinningRoll += 3
-  // } else {
-  //   minimumWinningRoll;
-  // }
-  
-//   if (instigatorRoll >= minimumWinningRoll) {
-//     return true;
-//   } else {
-//     return false;
-//   }
-// }
+export const character0 = new Character('freshman', 'nothing', 20)
+export const character1 = new Character('jock', 'strength', 30);
+export const character2 = new Character('nerd', 'robots', 10);
+export const character3 = new Character('criminal', 'switchblade', 30);
+export const character4 = new Character('artsy', 'intelligence', 15);
