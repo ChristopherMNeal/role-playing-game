723a4f59460afd7073ce4aec14ce5f2e39d61b6d
Christopher Neal <christopher.m.neal@gmail.com>
2021-11-10 14:05:26 -0800

    Pass first test, diceRoll() returns expected output
    Co-authored-by: Zach Wilson <zwilson22495@gmail.com>

--- src/js/character.js
+++ src/js/character.js
@@ -4,12 +4,41 @@ export default class Character {
     this.skill = skill;
     this.hp = hp;
   }
-  fight(character2) {
-    let instigatorWinner = (Math.floor(Math.random()) * 20) +1;
-    if (instigatorWinner > 10) {
-      return true;
-    } else {
-      return false;
-    }
-  }
 }
+
+
+// fight(instigator, instigatee) {
+//   let instigatorRoll = (Math.floor(Math.random()) * 20) +1;
+//   let minimumWinningRoll = 0;
+//   minimumWinningRoll = Math.floor(instigatee.hp/(instigator.hp + instigatee.hp)*20);
+
+  // if (instigator.includes(switchblade)) {
+  //   instigatorRoll += 2
+  // } else if (instigator.includes(strength)) {
+  //   instigatorRoll += 3
+  // } else if (instigator.includes(robots)) {
+  //   instigatorRoll += 5
+  // } else if (instigator.includes(intelligence)) {
+  //   instigatorRoll += 3
+  // } else { 
+  //   instigatorRoll;
+  // }
+
+  // if (instigatee.includes(switchblade)) {
+  //   minimumWinningRoll += 2
+  // } else if (instigatee.includes(strength)) {
+  //   minimumWinningRoll += 3
+  // } else if (instigatee.includes(robots)) {
+  //   minimumWinningRoll += 5
+  // } else if (instigatee.includes(intelligence)) {
+  //   minimumWinningRoll += 3
+  // } else {
+  //   minimumWinningRoll;
+  // }
+  
+//   if (instigatorRoll >= minimumWinningRoll) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
---
+++ src/js/game-play.js
@@ -0,0 +1,3 @@
+export function diceRoll() {
+  return (Math.floor(Math.random()) * 20) +1;
+}
