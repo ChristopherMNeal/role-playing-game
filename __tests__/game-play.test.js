6d7455529495d687c3971642f830f4346a4eda5b
Christopher Neal <christopher.m.neal@gmail.com>
2021-11-10 15:08:34 -0800

    Update and pass first test for fight()
    Co-authored-by: Zach Wilson <zwilson22495@gmail.com>

--- __tests__/game-play.test.js
+++ __tests__/game-play.test.js
@@ -1,6 +1,7 @@
 import {diceRoll} from '../src/js/game-play.js';
 import { fight } from '../src/js/game-play';
 import { character1 } from '../src/js/character';
+import { character2 } from '../src/js/character';
 import { character3 } from '../src/js/character';
 
 describe ('diceRoll', () => {
@@ -16,6 +17,8 @@ describe ('fight', () => {
     // const character1 = new Character('jock', 'strength', 30);
     // const character3 = new Character('criminal', 'switchblade', 30);
     const minimumWinningRollResult = fight(character1, character3);
+    const minimumWinningRollResult2 = fight(character1, character2);
     expect(minimumWinningRollResult).toBe(10);
+    expect(minimumWinningRollResult2).toBe(5);
   });
 });
