1b71fe9b99692572c79f9fa1f9a73222885af8bb
Christopher Neal <christopher.m.neal@gmail.com>
2021-11-10 10:02:30 -0800

    pass first test, character() returns a character class
    Co-authored-by: Zach Wilson <zwilson22495@gmail.com>

---
+++ src/js/character.js
@@ -0,0 +1,9 @@
+export default class Character {
+  constructor(type, skill) {
+    this.type = type;
+    this.skill = skill;
+  }
+  // getArea() {
+  //   return this.side1 * this.side2;
+  // }
+}
