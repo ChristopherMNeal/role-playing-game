c76cc41912c76a9555b2118aff57a62efb8f156a
Christopher Neal <christopher.m.neal@gmail.com>
2021-11-10 10:27:38 -0800

    update esLint rules
    Co-authored-by: Zach Wilson <zwilson22495@gmail.com>

--- __tests__/character.test.js
+++ __tests__/character.test.js
@@ -3,10 +3,14 @@ import Character from '../src/js/character.js';
 describe ('Character', () => {
   let character1;
   let character2;
+  let character3;
+  let character4;
 
   beforeEach(() => {
-    character1 = new Character('jock', 'strength');
-    character2 = new Character('nerd', 'robots');
+    character1 = new Character('jock', 'strength', 'thier parent');
+    character2 = new Character('nerd', 'robots', 'jock');
+    character3 = new Character('criminal', 'switchblade', 'authority figure');
+    character4 = new Character('artsy', 'intelligence', 'jock');
   });
 
   test('should correctly create two characters using given characteristics', () => {
--- src/js/character.js
+++ src/js/character.js
@@ -1,7 +1,8 @@
 export default class Character {
-  constructor(type, skill) {
+  constructor(type, skill, enemy) {
     this.type = type;
     this.skill = skill;
+    this.enemy = enemy;
   }
   // getArea() {
   //   return this.side1 * this.side2;
