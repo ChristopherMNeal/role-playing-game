c2def4f126cf2bcd0ef561b9adc0348f2c5598a4
Christopher Neal <christopher.m.neal@gmail.com>
2021-11-10 14:53:57 -0800

    Create first test for fight()

--- __tests__/game-play.test.js
+++ __tests__/game-play.test.js
@@ -1,6 +1,19 @@
 import {diceRoll} from '../src/js/game-play.js';
+import { fight } from '../src/js/game-play';
 
-test('Should return a number between 1 and 20', () => {
-  expect(diceRoll()).toBeGreaterThan(0);
-  expect(diceRoll()).toBeLessThan(20);
+describe ('diceRoll', () => {
+  test('Should return a number between 1 and 20', () => {
+    const result = diceRoll();
+    expect(result).toBeGreaterThan(0);
+    expect(result).toBeLessThan(20);
+  });
+});
+
+describe ('fight', () => {
+  const character1 = new Character('jock', 'strength', 30);
+  const character3 = new Character('criminal', 'switchblade', 30);
+  const minimumWinningRollResult = fight(character1, character3);
+  test('Should return a minimum winning roll based on character hps', () => {
+    expect(minimumWinningRollResult).toBe(10);
+  });
 });
--- src/js/game-play.js
+++ src/js/game-play.js
@@ -1,3 +1,34 @@
 export function diceRoll() {
   return (Math.floor(Math.random()) * 20) +1;
 }
+
+export fight(instigator, instigatee) {
+  // let instigatorRoll = diceRoll()
+  let minimumWinningRoll = 0;
+  // minimumWinningRoll = Math.floor(instigatee.hp/(instigator.hp + instigatee.hp)*20);
+  // return minimumWinningRoll
+  
+  // if (instigator.includes(switchblade)) {
+  //   instigatorRoll += 2
+  // } else if (instigator.includes(strength)) {
+  //   instigatorRoll += 3
+  // } else if (instigator.includes(robots)) {
+  //   instigatorRoll += 5
+  // } else if (instigator.includes(intelligence)) {
+  //   instigatorRoll += 3
+  // } else { 
+  //   instigatorRoll;
+  // }
+
+  // if (instigatee.includes(switchblade)) {
+  //   minimumWinningRoll += 2
+  // } else if (instigatee.includes(strength)) {
+  //   minimumWinningRoll += 3
+  // } else if (instigatee.includes(robots)) {
+  //   minimumWinningRoll += 5
+  // } else if (instigatee.includes(intelligence)) {
+  //   minimumWinningRoll += 3
+  // } else {
+  //   minimumWinningRoll;
+  // }
+}
