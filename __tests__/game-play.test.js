864fde006020ec0f7d7f08764676738a505564c4
Christopher Neal <christopher.m.neal@gmail.com>
2021-11-10 15:51:15 -0800

    Update again  and pass first test for fight(), achieve 100% testing coverage
    Co-authored-by: Zach Wilson <zwilson22495@gmail.com>

--- __tests__/game-play.test.js
+++ __tests__/game-play.test.js
@@ -1,8 +1,6 @@
 import {diceRoll} from '../src/js/game-play.js';
 import { fight } from '../src/js/game-play';
-import { character1 } from '../src/js/character';
-import { character2 } from '../src/js/character';
-import { character3 } from '../src/js/character';
+import { Character } from '../src/js/character';
 
 describe ('diceRoll', () => {
   test('Should return a number between 1 and 20', () => {
@@ -14,11 +12,40 @@ describe ('diceRoll', () => {
 
 describe ('fight', () => {
   test('Should return a minimum winning roll based on character hps', () => {
-    // const character1 = new Character('jock', 'strength', 30);
-    // const character3 = new Character('criminal', 'switchblade', 30);
+    const character1 = new Character('jock', '', 30);
+    const character2 = new Character('nerd', '', 10);
+    const character3 = new Character('criminal', '', 30);
     const minimumWinningRollResult = fight(character1, character3);
     const minimumWinningRollResult2 = fight(character1, character2);
     expect(minimumWinningRollResult).toBe(10);
     expect(minimumWinningRollResult2).toBe(5);
   });
+
+  test('Should take switchblade into account of instigatorRoll', () => {
+    const character0 = new Character ('freshman', 'nothing', 10);
+    const character1 = new Character ('nerd', 'switchblade', 10);
+    const character2 = new Character ('criminal', 'strength', 10);
+    const character3 = new Character('jock', 'robots', 10);
+    const character4 = new Character('artsy', 'intelligence', 10);
+
+    const minimumWinningRollResult1a = fight(character1, character0);
+    const minimumWinningRollResult2a = fight(character2, character0);
+    const minimumWinningRollResult3a = fight(character3, character0);
+    const minimumWinningRollResult4a = fight(character4, character0);
+    const minimumWinningRollResult1b = fight(character0, character1);
+    const minimumWinningRollResult2b = fight(character0, character2);
+    const minimumWinningRollResult3b = fight(character0, character3);
+    const minimumWinningRollResult4b = fight(character0, character4);
+
+    expect(minimumWinningRollResult1a).toBe(8);
+    expect(minimumWinningRollResult2a).toBe(7);
+    expect(minimumWinningRollResult3a).toBe(5);
+    expect(minimumWinningRollResult4a).toBe(7);
+
+    expect(minimumWinningRollResult1b).toBe(12);
+    expect(minimumWinningRollResult2b).toBe(13);
+    expect(minimumWinningRollResult3b).toBe(15);
+    expect(minimumWinningRollResult4b).toBe(13);
+  });
 });
+
--- src/js/game-play.js
+++ src/js/game-play.js
@@ -3,32 +3,31 @@ export function diceRoll() {
 }
 
 export function fight(instigator, instigatee) {
-  // let instigatorRoll = diceRoll()
   let minimumWinningRoll = 0;
   minimumWinningRoll = Math.floor(instigatee.hp/(instigator.hp + instigatee.hp)*20);
+  
+  if (instigator.skill === ('switchblade')) {
+    minimumWinningRoll -= 2;
+  } else if (instigator.skill === ('strength')) {
+    minimumWinningRoll -= 3;
+  } else if (instigator.skill === ('robots')) {
+    minimumWinningRoll -= 5;
+  } else if (instigator.skill === ('intelligence')) {
+    minimumWinningRoll -= 3;
+  } else { 
+    minimumWinningRoll;
+  }
+
+  if (instigatee.skill === ('switchblade')) {
+    minimumWinningRoll += 2;
+  } else if (instigatee.skill === ('strength')) {
+    minimumWinningRoll += 3;
+  } else if (instigatee.skill === ('robots')) {
+    minimumWinningRoll += 5;
+  } else if (instigatee.skill === ('intelligence')) {
+    minimumWinningRoll += 3;
+  } else {
+    minimumWinningRoll;
+  }
   return minimumWinningRoll;
-
-  // if (instigator.includes(switchblade)) {
-  //   instigatorRoll += 2
-  // } else if (instigator.includes(strength)) {
-  //   instigatorRoll += 3
-  // } else if (instigator.includes(robots)) {
-  //   instigatorRoll += 5
-  // } else if (instigator.includes(intelligence)) {
-  //   instigatorRoll += 3
-  // } else { 
-  //   instigatorRoll;
-  // }
-
-  // if (instigatee.includes(switchblade)) {
-  //   minimumWinningRoll += 2
-  // } else if (instigatee.includes(strength)) {
-  //   minimumWinningRoll += 3
-  // } else if (instigatee.includes(robots)) {
-  //   minimumWinningRoll += 5
-  // } else if (instigatee.includes(intelligence)) {
-  //   minimumWinningRoll += 3
-  // } else {
-  //   minimumWinningRoll;
-  // }
 }
